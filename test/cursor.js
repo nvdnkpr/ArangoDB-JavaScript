06bf69b42ffd74d7a77817d3b57d2184b5c97d99
scottashton <f.bartels@triagens.de>
2014-06-23 08:37:06 +0200

    cherrypick

--- test/cursor.js
+++ test/cursor.js
@@ -213,6 +213,19 @@ describe("cursor", function() {
         });
     })
 
+    it('using query string', function(done) {
+      this.timeout(50000);
+      var query = db.query.string("FOR u IN @@collection RETURN u");
+      query.exec({
+        '@collection': 'newCollection'
+      }, function(err, ret) {
+        check(done, function() {
+          ret.error.should.equal(false);
+          ret.code.should.equal(201);
+        });
+      });
+    })
+
     it('using query module', function(done) {
         var query = db.query.
         for ('u'). in ('@@collection').
